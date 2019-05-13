24808f279406daeb73b8616b6a1a2ab39034a71c
xie123aa <912392651@qq.com>
2019-05-14 01:29:35 +0800

    字段导入成功

--- src/main/java/deal/CompareExcel.java
+++ src/main/java/deal/CompareExcel.java
@@ -1,8 +1,23 @@
 package deal;
 
 import exceltools.ExcelUtil;
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  * @Author: georgexie
@@ -11,18 +26,109 @@ import java.util.List;
  */
 public class CompareExcel {
     public static void main(String[] args) {
-        String path = "C:\\Users\\Administrator\\Desktop\\补数据\\20190508导入数据.xls";
+    String filePath = "C:\\补数据\\昨天的\\20190508导入数据.xlsx";
+    Workbook wb =null;
+    Sheet sheet = null;
+    Row row = null;
+    List<Map<String,String>> list = null;
+    String cellData = null;
+    String columns[] = { "申诉信息流水号","改派流水号","并案流水号","撤诉流水号","转办编号","调解流水号",
+            "用户姓名","联系电话","申诉涉及号码","通讯地址","工作单位","电子邮箱","投诉编码","主投企业",
+            "涉及企业","所属省份","地级市","分类码(一)","分类码(二)","分类码(三)","业务码(一)","业务码(二)",
+            "业务码(三)","申诉来源","申诉类型","认定员","调解员","受理员","处理方式","申诉状态","认定结果","受理和解结果","转办和解结果","立案和解结果","是否并案",
+            "申诉日期","受理日期","转办日期","调解日期","结案日期","预处理日期","申诉内容",
+            "认证员审核","不受理原因","自动单备注","审核意见","反馈结果","","","",""};
+    wb = readExcel(filePath);
+        if(wb != null){
+        //用来存放表中数据
+        list = new ArrayList<Map<String,String>>();
+        //获取第一个sheet
+        sheet = wb.getSheetAt(0);
+        //获取最大行数
+        int rownum = sheet.getPhysicalNumberOfRows();
+        //获取第一行
+        row = sheet.getRow(0);
+        //获取最大列数
+        int colnum = row.getPhysicalNumberOfCells();
+        for (int i = 1; i<rownum; i++) {
+            Map<String,String> map = new LinkedHashMap<String,String>();
+            row = sheet.getRow(i);
+            if(row !=null){
+                for (int j=0;j<colnum;j++){
+                    cellData = (String) getCellFormatValue(row.getCell(j));
+                    map.put(columns[j], cellData);
+                }
+            }else{
+                break;
+            }
+            list.add(map);
+        }
+    }
+    //遍历解析出来的list
+        for (Map<String,String> map : list) {
+        for (Map.Entry<String,String> entry : map.entrySet()) {
+            System.out.print(entry.getKey()+":"+entry.getValue()+",");
+        }
+        System.out.println();
+    }
+
+}
+    //读取excel
+    public static Workbook readExcel(String filePath){
+        Workbook wb = null;
+        if(filePath==null){
+            return null;
+        }
+        String extString = filePath.substring(filePath.lastIndexOf("."));
+        InputStream is = null;
         try {
-            List<List<String>> result = new ExcelUtil().readXls(path);
-            System.out.println(result.size());
-            for (int i = 0; i < result.size(); i++) {
-                List<String> model = result.get(i);
-                System.out.println("orderNum:" + model.get(0) + "--> orderAmount:" + model.get(1));
+            is = new FileInputStream(filePath);
+            if(".xls".equals(extString)){
+                return wb = new HSSFWorkbook(is);
+            }else if(".xlsx".equals(extString)){
+                return wb = new XSSFWorkbook(is);
+            }else{
+                return wb = null;
             }
 
-        } catch (Exception e) {
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        return wb;
+    }
+    public static Object getCellFormatValue(Cell cell){
+        Object cellValue = null;
+        if(cell!=null){
+            //判断cell类型
+            switch(cell.getCellType()){
+                case Cell.CELL_TYPE_NUMERIC:{
+                    cellValue = String.valueOf(cell.getNumericCellValue());
+                    break;
+                }
+                case Cell.CELL_TYPE_FORMULA:{
+                    //判断cell是否为日期格式
+                    if(DateUtil.isCellDateFormatted(cell)){
+                        //转换为日期格式YYYY-mm-dd
+                        cellValue = cell.getDateCellValue();
+                    }else{
+                        //数字
+                        cellValue = String.valueOf(cell.getNumericCellValue());
+                    }
+                    break;
+                }
+                case Cell.CELL_TYPE_STRING:{
+                    cellValue = cell.getRichStringCellValue().getString();
+                    break;
+                }
+                default:
+                    cellValue = "";
+            }
+        }else{
+            cellValue = "";
+        }
+        return cellValue;
     }
 
 }
